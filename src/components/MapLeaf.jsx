9d87335207bb939402c01f92b1ce4eb8936b875c
paul-dobre <paul.dobre@ucalgary.ca>
2024-01-12 17:32:10 -0700

    Added mapbox link

--- src/components/MapLeaf.jsx
+++ src/components/MapLeaf.jsx
@@ -3,11 +3,12 @@ import "leaflet/dist/leaflet.css"
 import { MapContainer, TileLayer, Marker, Popup,useMap } from "react-leaflet";
 
 const MapLeaf = () => {
+  const code = "https://api.mapbox.com/styles/v1/ypaul/clrbbh2e4002d01re65us9mvk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoieXBhdWwiLCJhIjoiY2xyYjV5eHZqMGtxajJpcnN4Zm1kOGU4YSJ9.nkPc81nO8sFFfAEin7TN1w";
   return (
     <MapContainer center={[51, -114]} zoom={13} scrollWheelZoom={false} className={`flex md:flex-row flex-col w-[1000px] h-[800px] justify-center`}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url={code}
       />
       <Marker position={[51, -114]}>
       </Marker>
