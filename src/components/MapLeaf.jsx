a6d8149e7e659a8aede7d540570e6a59fee29d21
paul-dobre <paul.dobre@ucalgary.ca>
2024-01-14 17:08:28 -0700

    Basic routing and directions completed

--- src/components/MapLeaf.jsx
+++ src/components/MapLeaf.jsx
@@ -1,18 +1,100 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer, Marker, Popup,useMap } from "react-leaflet";
+import L from "leaflet";
+import {Icon} from "leaflet"
+import * as esri from 'esri-leaflet';
+import * as vec from 'esri-leaflet-vector';
+import {request} from '@esri/arcgis-rest-request'
+import{solveRoute} from '@esri/arcgis-rest-routing'
+import { ApiKeyManager } from "@esri/arcgis-rest-request";
+
+
+const customIcon = new Icon({
+  iconUrl: "CustomMarker.svg",
+  iconSize: [38,38]
+});
 
 const MapLeaf = () => {
-  const code = "https://api.mapbox.com/styles/v1/ypaul/clrbbh2e4002d01re65us9mvk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoieXBhdWwiLCJhIjoiY2xyYjV5eHZqMGtxajJpcnN4Zm1kOGU4YSJ9.nkPc81nO8sFFfAEin7TN1w";
+  
+  useEffect(() => {
+    // Create the map
+    const map = L.map("map", { minZoom: 2 }).setView([51, -114], 13);
+
+    // Add basemap
+    const apiKey = "AAPK3c3f7569a5364ebf989232a728f5cbbbD0PGCXGZqbFvXv3e1oUb76gUENrlq1_yhMDPKhunJRKWbLKb2OdXPodGKWPO3UkL";
+    vec.vectorBasemapLayer("arcgis/navigation-night", { apiKey: apiKey }).addTo(map);
+
+    const directions = document.createElement("div");
+    directions.id = "directions";
+    directions.innerHTML = "Click on the map to create a start and end for the route.";
+    document.body.appendChild(directions);
+
+    const startLayerGroup = L.layerGroup().addTo(map);
+    const endLayerGroup = L.layerGroup().addTo(map);
+    const routeLines = L.layerGroup().addTo(map);
+
+    let currentStep = "start";
+    let startCoords, endCoords;
+
+
+    const updateRoute = () => 
+    {
+      const authentication = ApiKeyManager.fromKey(apiKey);
+      solveRoute({
+        stops: [startCoords, endCoords],
+        endpoint: "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/solve",
+        authentication
+      })
+      .then((response) =>{
+        routeLines.clearLayers();
+        L.geoJSON(response.routes.geoJson).addTo(routeLines);
+
+        const directionsHTML = response.directions[0].features.map((f) => f.attributes.text).join("<br/>");
+        directions.innerHTML = directionsHTML;
+        startCoords = null;
+        endCoords = null;
+      })  
+      .catch((error) => {
+        console.error(error);
+        alert("There was a problem using the route service. See the console for details.");
+      });
+    };
+
+    map.on("click", (e) => {
+
+      const coordinates = [e.latlng.lng, e.latlng.lat];
+
+      if (currentStep === "start"){
+        startLayerGroup.clearLayers();
+        endLayerGroup.clearLayers();
+        routeLines.clearLayers();
+
+        L.marker(e.latlng, {icon: customIcon}).addTo(startLayerGroup);
+        startCoords = coordinates;
+        currentStep = "end";
+      } else {
+        L.marker(e.latlng, {icon: customIcon}).addTo(endLayerGroup);
+        endCoords = coordinates;
+
+        currentStep = "start";
+      }
+
+      if (startCoords && endCoords){
+        updateRoute();
+      }
+    });
+
+
+    return () => {
+      map.off("click")
+      map.remove();
+    };
+  }, []);
+
+
   return (
-    <MapContainer center={[51, -114]} zoom={13} scrollWheelZoom={false} className={`flex md:flex-row flex-col w-[1000px] h-[800px] justify-center`}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url={code}
-      />
-      <Marker position={[51, -114]}>
-      </Marker>
-    </MapContainer>
+    
+    <div id="map" className={`flex md:flex-row flex-col w-[1000px] h-[800px] justify-center`}></div>
   );
 };
 
