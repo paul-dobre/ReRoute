a45f0521804e371945aed7d5b6a0e8fe19b57cf2
paul-dobre <paul.dobre@ucalgary.ca>
2024-01-26 12:09:10 -0700

    Route around barriers

--- src/components/MapLeaf.jsx
+++ src/components/MapLeaf.jsx
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from "react";
 import "leaflet/dist/leaflet.css"
-import L from "leaflet";
+import L, { polyline } from "leaflet";
 import {Icon} from "leaflet"
 import * as esri from 'esri-leaflet';
 import * as vec from 'esri-leaflet-vector';
-import {request} from '@esri/arcgis-rest-request'
-import{solveRoute} from '@esri/arcgis-rest-routing'
 import { ApiKeyManager } from "@esri/arcgis-rest-request";
+import PolygonBarrier from "@arcgis/core/rest/support/PolygonBarrier"
+import PolyLine from '@arcgis/core/geometry/Polyline'
+import Graphic from '@arcgis/core/Graphic'
 
 
 const customIcon = new Icon({
@@ -18,7 +19,8 @@ const MapLeaf = () => {
   
   useEffect(() => {
     // Create the map
-    const map = L.map("map", { minZoom: 2 }).setView([51, -114], 13);
+    const map = L.map("map", { minZoom: 2 }).setView([42.5,-96.44], 13);
+    
 
     // Add basemap
     const apiKey = "AAPK3c3f7569a5364ebf989232a728f5cbbbD0PGCXGZqbFvXv3e1oUb76gUENrlq1_yhMDPKhunJRKWbLKb2OdXPodGKWPO3UkL";
@@ -37,27 +39,59 @@ const MapLeaf = () => {
     let startCoords, endCoords;
 
 
-    const updateRoute = () => 
+    const updateRoute = async (url) => 
     {
       const authentication = ApiKeyManager.fromKey(apiKey);
-      solveRoute({
-        stops: [startCoords, endCoords],
-        endpoint: "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/solve",
-        authentication
-      })
-      .then((response) =>{
-        routeLines.clearLayers();
-        L.geoJSON(response.routes.geoJson).addTo(routeLines);
 
-        const directionsHTML = response.directions[0].features.map((f) => f.attributes.text).join("<br/>");
-        directions.innerHTML = directionsHTML;
+      try {
+      const response = await fetch(url)
+
+      if (!response.ok) {
+
+        // Assuming the API returns an error message in the response body
+        const errorData = await response.json();
+        const errorMessage = errorData.message || response.statusText;
+        throw new Error(`API error: ${errorMessage}`);
+
+      }
+        routeLines.clearLayers();
+        const ResponseData = await response.json();
+        console.log(ResponseData.polygonBarriers.features[0].geometry.rings[0]);
+
+        let polylineSymbol = {
+          type: "simple-line",  // autocasts as SimpleLineSymbol()
+          color: [226, 119, 40],
+          width: 4
+        };
+        var coords = ResponseData.routes.features[0].geometry.paths[0];
+
+        for (let i = 0; i < coords.length; i++) {
+          const temp = coords[i][0];
+          coords[i][0] = coords[i][1];
+          coords[i][1] = temp;
+      }
+
+      var polycoords = ResponseData.polygonBarriers.features[0].geometry.rings[0];
+
+      for (let i = 0; i < polycoords.length; i++) {
+        const temp = polycoords[i][0];
+        polycoords[i][0] = polycoords[i][1];
+        polycoords[i][1] = temp;
+      }
+      
+        L.polygon(polycoords, {color: 'red'}).addTo(map);
+
+        L.polyline(coords, {color: 'red'}).addTo(map);
+
+
+        //const directionsHTML = response.directions[0].features.map((f) => f.attributes.text).join("<br/>");
+        //directions.innerHTML = directionsHTML;
         startCoords = null;
         endCoords = null;
-      })  
-      .catch((error) => {
+      } catch(error) {
         console.error(error);
         alert("There was a problem using the route service. See the console for details.");
-      });
+      };
     };
 
     map.on("click", (e) => {
@@ -80,7 +114,8 @@ const MapLeaf = () => {
       }
 
       if (startCoords && endCoords){
-        updateRoute();
+        const url = "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/solve?returnPolygonBarriers=true&outSR=4326&f=json&polygonBarriers=%7B%22features%22%3A%5B%7B%22geometry%22%3A%7B%22rings%22%3A%5B%5B%5B-96.382%2C42.49%5D%2C%5B-96.391%2C42.471%5D%2C%5B-96.414%2C42.475%5D%2C%5B-96.421%2C42.491%5D%2C%5B-96.401%2C42.505%5D%2C%5B-96.382%2C42.49%5D%5D%5D%7D%2C%22attributes%22%3A%7B%22Name%22%3A%22Flood%20zone%22%2C%22BarrierType%22%3A0%7D%7D%5D%7D&token=AAPK3c3f7569a5364ebf989232a728f5cbbbD0PGCXGZqbFvXv3e1oUb76gUENrlq1_yhMDPKhunJRKWbLKb2OdXPodGKWPO3UkL&stops=-96.436378,42.499344;-96.364166,42.459969&startTime=now&returnDirections=true"
+        updateRoute(url);
       }
     });
 
